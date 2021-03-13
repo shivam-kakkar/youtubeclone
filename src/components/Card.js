868aba234ddcf7cd235dd721e8fd8f989c7e30c5
Shivam Kakkar <shivamkakkar4@gmail.com>
2021-03-14 01:12:31 +0530

    Created Video Player and integrated it

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,30 +1,38 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, Dimensions } from "react-native";
+import { StyleSheet, Text, View, Image, Dimensions, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 const Card = props => {
+  const navigation = useNavigation();
   return (
-    <View style={{ marginBottom: 10 }}>
-      <Image
-        source={{
-          uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
-        }}
-        style={{ width: "100%", height: 200 }}
-      />
-      <View style={{ flexDirection: "row", margin: 5 }}>
-        <MaterialIcons name="account-circle" size={40} color="#212121" />
-        <View style={{ marginLeft: 10 }}>
-          <Text
-            style={{ fontSize: 20, width: Dimensions.get("screen").width - 50 }}
-            ellipsizeMode="tail"
-            numberOfLines={2}
-          >
-            {props.title}
-          </Text>
-          <Text>{props.channel}</Text>
+    <TouchableOpacity
+      onPress={() =>
+        navigation.navigate("videoplayer", { videoId: props.videoId, title: props.title })
+      }
+    >
+      <View style={{ marginBottom: 10 }}>
+        <Image
+          source={{
+            uri: `https://i.ytimg.com/vi/${props.videoId}/hqdefault.jpg`,
+          }}
+          style={{ width: "100%", height: 200 }}
+        />
+        <View style={{ flexDirection: "row", margin: 5 }}>
+          <MaterialIcons name="account-circle" size={40} color="#212121" />
+          <View style={{ marginLeft: 10 }}>
+            <Text
+              style={{ fontSize: 20, width: Dimensions.get("screen").width - 50 }}
+              ellipsizeMode="tail"
+              numberOfLines={2}
+            >
+              {props.title}
+            </Text>
+            <Text>{props.channel}</Text>
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
--- src/screens/VideoPlayer.js
+++ src/screens/VideoPlayer.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { WebView } from "react-native-webview";
 import Constant from "expo-constants";
 
-const VideoPlayer = () => {
+const VideoPlayer = ({ route }) => {
+  const { videoId, title } = route.params;
   return (
     <View style={{ flex: 1, marginTop: Constant.statusBarHeight }}>
-      <Text>VideoPlayer</Text>
+      <View style={{ width: "100%", height: 200 }}>
+        <WebView
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          source={{ uri: `https://www.youtube.com/embed/${videoId}` }}
+        />
+      </View>
+      <Text
+        style={{ fontSize: 20, width: Dimensions.get("screen").width - 50, margin: 9 }}
+        numberOfLines={2}
+        ellipsizeMode="tail"
+      >
+        {title}
+      </Text>
+      <View style={{ borderBottomWidth: 1 }} />
     </View>
   );
 };
